57dc7291b0b9137251f0a5c49b513e3afdaf22ba
FireHogGames <hidde01@live.nl>
2019-06-05 13:26:37 +0200

    Started the project

---
+++ SALSA/SALSA/Logging.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SALSA
+{
+    public enum LogLevel
+    {
+        info,
+        warning,
+        error
+    }
+
+    //Make logging errors and communicating with the user easier
+    class Logging
+    {
+        
+        public void Log(LogLevel level, string message)
+        {
+            //Zero means info
+            if(level == LogLevel.info)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+            //One means warning
+            if(level == LogLevel.warning)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+            //Two means Error
+            if(level == LogLevel.error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ResetColor();
+                Console.WriteLine("Press any key to close the console...");
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
---
+++ SALSA/SALSA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SALSA
+{
+    class Program
+    {
+        //Constructor which starts the program
+        public Program()
+        {
+            //Tell the user SALSA is initializing
+            Logging logger = new Logging();
+            logger.Log(LogLevel.info, "Initializing SALSA...");
+            Run();
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        private void Run()
+        {
+            string arg = "";
+
+            while (arg != "C")
+            {
+                arg = Console.ReadLine();
+            }
+
+            return;
+        }
+    }
+}
